7c4fa3fd8552f93527e4627cfa122db5d6638537
PatrickKish1 <patrickkesh90@gmail.com>
2024-09-16 08:24:15 -0800

    Updating files

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,16 +5,33 @@ import {HousingLocation} from './housinglocation';
 })
 export class HousingService {
   url = 'http://localhost:3000/locations';
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
   async getAllHousingLocations(): Promise<HousingLocation[]> {
     const data = await fetch(this.url);
-    return (await data.json()) ?? [];
+    const locations = (await data.json()) ?? [];
+    return this.processLocations(locations);
   }
+
   async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
-    return (await data.json()) ?? {};
+    const location = (await data.json()) ?? {};
+    return this.processLocation(location);
   }
+
   submitApplication(firstName: string, lastName: string, email: string) {
     // tslint:disable-next-line
     console.log(firstName, lastName, email);
+  }
+
+  private processLocations(locations: any[]): HousingLocation[] {
+    return locations.map(location => this.processLocation(location));
+  }
+
+  private processLocation(location: any): HousingLocation {
+    if (location.photo) {
+      location.photo = location.photo.replace('${this.baseUrl}', this.baseUrl);
+    }
+    return location;
   }
 }
